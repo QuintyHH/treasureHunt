59a17cde8eaa0601afb2d377236a76dd55db5af9
QuintyHH <razmocanu@gmail.com>
2021-03-14 12:39:44 +0000

    added general error handling

--- newParser.js
+++ newParser.js
@@ -1,8 +1,11 @@
 const fs = require('fs')
 const pf = require('pathfinding')
+const FILE_PATH = process.argv[2] // Here I am setting the filepath I need processed
+
+const SUCCESS = 'success'
+const FAIL = 'fail'
 const CONSOLE_YELLOW = '\x1b[33m%s\x1b[0m'
 const CONSOLE_RED = '\x1b[31m%s\x1b[0m'
-const FILE_PATH = process.argv[2] // Here I am setting the filepath I need processed
 
 const pipe = (...fns) => (value) => fns.reduce((value, fn) => fn(value), value)
 
@@ -91,33 +94,40 @@ const formatMap = (mapObj) => {
 }
 
 const renderMap = (map) => {
-  // console.clear()
+  console.clear()
   return console.table(map)
 }
 
-const renderNotification = (path) =>
-  path
-    ? console.log(CONSOLE_YELLOW, 'Treasure was found! Argghhh matey!')
-    : console.log(CONSOLE_RED, 'No treasure here! Back to the ship!')
+const renderNotification = (type = FAIL, path = false) => {
+  return type === SUCCESS
+    ? path
+      ? console.log(CONSOLE_YELLOW, 'Treasure was found! Argghhh matey!')
+      : console.log(CONSOLE_RED, 'No treasure here! Back to the ship!')
+    : console.log(CONSOLE_RED, 'Treasure map could not be deciphered!')
+}
 
 const main = (file) => {
-  // This is our drawn map with S as start E as treasure X for reef and . for spaces where we can add our steps.
-  const treasureMap = pipe(
-    getFile, // Here I read the file that I needed
-    sanitizeFile, // Here I make sure the file contents are compatible with the required format
-    generateMatrix, // Here I build the map matrix
-    setStart, // Here I set the start position
-    setTreasure, // Here I set the treasure position
-    setCoordinates, // Here I prepare the coordinates to be able to set minMax on axis
-    setMapMax, // Here I set the maps' max coordinates
-    buildGrid, // And finally, I build a grid as an array of arrays based on max lengths
-    findPath, // Here I use the pathfinding lib to apply A* algorithm
-    formatMap // Here I transform the map to match requirements
-  )(file)
+  try {
+    // This is our drawn map with S as start E as treasure X for reef and . for spaces where we can navigate. 0 is marking the found path
+    const treasureMap = pipe(
+      getFile, // Here I read the file that I needed
+      sanitizeFile, // Here I make sure the file contents are compatible with the required format
+      generateMatrix, // Here I build the map matrix
+      setStart, // Here I set the start position
+      setTreasure, // Here I set the treasure position
+      setCoordinates, // Here I prepare the coordinates to be able to set minMax on axis
+      setMapMax, // Here I set the maps' max coordinates
+      buildGrid, // And finally, I build a grid as an array of arrays based on max lengths
+      findPath, // Here I use the pathfinding lib to apply A* algorithm
+      formatMap // Here I transform the map to match requirements
+    )(file)
 
-  const { map, path } = treasureMap
-  renderMap(map)
-  renderNotification(path)
+    const { map, path } = treasureMap
+    renderMap(map)
+    renderNotification(SUCCESS, path)
+  } catch (err) {
+    renderNotification(FAIL)
+  }
 }
 
 main(FILE_PATH)
